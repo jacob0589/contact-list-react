fe4e806ce4d55bb6d083b070f8be89a7c50b032a
jacob0589 <alonsocb8995@gmail.com>
2023-03-07 05:51:50 +0000

    avance

--- src/front/js/pages/addContact.jsx
+++ src/front/js/pages/addContact.jsx
@@ -8,17 +8,41 @@ const AddContact = () => {
         full_name: "Antonio",
         email: "antonio@antonio.com",
         phone: "2222",
-        agenda_slug: "agenda_de_antonio",
+        agenda_slug: "agenda_de_Alonso",
         address: "47568 NW 34ST, 33434 FL, USA",
+    }
+
+    const registerContact = (e) => {
+        e.preventDefault()
+        let data = new FormData(e.target)
+        newContact = {
+            ...newContact, full_name: data.get("full_name"),
+            email: data.get("email"),
+            phone: data.get("phone"),
+            agenda_slug: "agenda_de_Alonso",
+            address: data.get("address"),  };
+
+           console.log(newContact)
+
+    
+     
+
     }
 
     return (<div>Aquí debería agregar contactos nuevos
         <br />
         <Link to="/">Regresar a lista de contactos</Link>
         <br />
-        <button type="button" onClick={() => {
-            actions.addContact(newContact)
-        }}>Agregar Contacto a la Agenda</button>
+        <form onSubmit={registerContact}>
+            <input type="text" placeholder="FULL NAME" name="full_name"></input>
+            <input type="text" placeholder="EMAIL" name="email"></input>
+            <input type="text" placeholder="PHONE" name="phone"></input>
+            <input type="text" placeholder="ADDRESS" name="address"></input>
+            <button type="submit">Enviar Contacto</button>
+        </form>
+
+
+
     </div>)
 }
 
