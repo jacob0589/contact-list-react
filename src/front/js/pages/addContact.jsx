691cbcc05dcd2a5a708dd4b4c62b8b92f160cb32
jacob0589 <alonsocb8995@gmail.com>
2023-03-10 20:57:35 +0000

    avance1

--- src/front/js/pages/addContact.jsx
+++ src/front/js/pages/addContact.jsx
@@ -1,49 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 const AddContact = () => {
-    const { store, actions } = useContext(Context)
-    let newContact = {
-        full_name: "Antonio",
-        email: "antonio@antonio.com",
-        phone: "2222",
-        agenda_slug: "agenda_de_Alonso",
-        address: "47568 NW 34ST, 33434 FL, USA",
-    }
+    const { store, actions } = useContext(Context);
+    const full_name_ref = useRef();
+    const email_ref = useRef();
+    const phone_ref = useRef();
+    const address_ref = useRef();
 
     const registerContact = (e) => {
-        e.preventDefault()
-        let data = new FormData(e.target)
-        newContact = {
-            ...newContact, full_name: data.get("full_name"),
-            email: data.get("email"),
-            phone: data.get("phone"),
+        e.preventDefault();
+        const newContact = {
+            full_name: full_name_ref.current.value,
+            email: email_ref.current.value,
+            phone: phone_ref.current.value,
+            address: address_ref.current.value,
             agenda_slug: "agenda_de_Alonso",
-            address: data.get("address"),  };
+        };
+        actions.addContact(newContact);
+        e.target.reset();
+    };
 
-           console.log(newContact)
-
-    
-     
-
-    }
-
-    return (<div>Aquí debería agregar contactos nuevos
-        <br />
-        <Link to="/">Regresar a lista de contactos</Link>
-        <br />
-        <form onSubmit={registerContact}>
-            <input type="text" placeholder="FULL NAME" name="full_name"></input>
-            <input type="text" placeholder="EMAIL" name="email"></input>
-            <input type="text" placeholder="PHONE" name="phone"></input>
-            <input type="text" placeholder="ADDRESS" name="address"></input>
-            <button type="submit">Enviar Contacto</button>
-        </form>
-
-
-
-    </div>)
-}
+    return (
+        <div>
+            Aquí debería agregar contactos nuevos
+            <br />
+            <Link to="/">Regresar a lista de contactos</Link>
+            <br />
+            <form onSubmit={registerContact}>
+                <input type="text" placeholder="FULL NAME" ref={full_name_ref}></input>
+                <input type="text" placeholder="EMAIL" ref={email_ref}></input>
+                <input type="text" placeholder="PHONE" ref={phone_ref}></input>
+                <input type="text" placeholder="ADDRESS" ref={address_ref}></input>
+                <button type="submit">Enviar Contacto</button>
+            </form>
+        </div>
+    );
+};
 
 export default AddContact;
